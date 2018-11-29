afe07161994d8e5456cad6439d70312366d6dad3
linco <cornelia.lin@in.tum.de>
2018-11-29 15:52:10 +0100

    Moved down brain loading and ensuring it only happens once with boolean

--- __init__.py
+++ __init__.py
@@ -7,6 +7,7 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from globals import *                               # noqa: E402
 from utils import EmbeddedBot                       # noqa: E402
 from mycroft.skills.core import FallbackSkill       # noqa: E402
+from mycroft.util.log import LOG                    # noqa: E402
 
 
 __author__ = 'Fantailed'
@@ -18,15 +19,21 @@ class FallbackChatbot(FallbackSkill):
         super(FallbackChatbot, self).__init__(name="FallbackChatbot")
         self.aiml_bot = None
         self.client_context = None
+        self.brain_loaded = False
 
     def initialize(self):
+        # 0 is high priority, 100 is low
+        self.register_fallback(self.handle_fallback, priority=90)
+
+    def load_brain(self):
+        LOG.info('Loading Brain')
         os.chdir(SKILL_PATH)
         # Initialize inner bot object
         self.aiml_bot = EmbeddedBot('config.yaml')
         self.client_context = self.aiml_bot.create_client_context("mycroft")
         os.chdir(MYCROFT_WORKING_DIR)
-        # 0 is high priority, 100 is low
-        self.register_fallback(self.handle_fallback, priority=90)
+
+        self.brain_loaded = True
 
     def ask_brain(self, utterance):
         response = self.aiml_bot.process_question(self.client_context, utterance)
@@ -36,6 +43,8 @@ class FallbackChatbot(FallbackSkill):
         if not self.settings.get("enabled") or self.settings.get("enabled") == 'true':
             if not self.settings.get("enabled"):
                 print("Warning: Bot does not have a setting called 'enabled'!")
+            if not self.brain_loaded:
+                self.load_brain()
             utterance = message.data.get("utterance")
             answer = self.ask_brain(utterance)
             if answer != "":
